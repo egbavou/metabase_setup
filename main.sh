a805a662e6ca38eb15ca7cce98d492c692c1f7d1
egbavou <egbavou32@gmail.com>
2025-04-18 03:06:39 +0100

    improve main script

--- main.sh
+++ main.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]; then 
+  echo "You need to run this script as root."
+  exit 1
+fi
+
 PATH="/tmp/metabase_setup/"
 MYSQL_BASH_FILE_NAME="mysql.sh"
 METABASE_BASH_FILE_NAME="metabase.sh"
 
-cd $PATH
+cd "$SETUP_PATH" || { echo "The folder $SETUP_PATH doesn't exist."; exit 1; }
 
-sudo chmod +x "$PATH_SETUP/$MYSQL_BASH_FILE_NAME"
-sudo chmod +x "$PATH_SETUP/$METABASE_BASH_FILE_NAME"
-sudo "$PATH_SETUP/$MYSQL_BASH_FILE_NAME"
-sudo "$PATH_SETUP/$METABASE_BASH_FILE_NAME"
+sudo -i
+
+chmod +x "$PATH_SETUP/$MYSQL_BASH_FILE_NAME"
+chmod +x "$PATH_SETUP/$METABASE_BASH_FILE_NAME"
+"$PATH_SETUP/$MYSQL_BASH_FILE_NAME"
+"$PATH_SETUP/$METABASE_BASH_FILE_NAME"
